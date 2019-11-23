4360b38b7c71581a81989c26cff1d3e911f375fc
matcalu <Kop4fde!>
2019-11-23 16:06:32 +0100

    Initial commit

---
+++ src/ConsoleApp.java
@@ -0,0 +1,9 @@
+public class ConsoleApp {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+        int c = a + b;
+
+        System.out.println("Wynik dodawania " + a + " do " + b + " to: " + c);
+    }
+}
